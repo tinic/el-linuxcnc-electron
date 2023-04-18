cf0ee05a4a817bb60534d6c042c5f9499955d77f
Tinic Uro <tinicuro@gmail.com>
2023-04-17 20:21:39 -0700

    Use formatter on python.

--- elle-app/elle-hal/lathe_display.py
+++ elle-app/elle-hal/lathe_display.py
@@ -19,38 +19,49 @@ c = linuxcnc.command()
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
+
 class NullProgress:
-    def nextphase(self, var1): pass
-    def progress(self): pass
+    def nextphase(self, var1):
+        pass
+
+    def progress(self):
+        pass
+
 
 class StatCanon(rs274.glcanon.GLCanon, rs274.interpret.StatMixin):
-    def __init__(self, colors, geometry, is_foam, lathe_view_option, stat, random, arcdivision):
+    def __init__(
+        self, colors, geometry, is_foam, lathe_view_option, stat, random, arcdivision
+    ):
         rs274.glcanon.GLCanon.__init__(self, colors, geometry)
         rs274.interpret.StatMixin.__init__(self, stat, random)
         self.progress = NullProgress()
         self.lathe_view_option = lathe_view_option
         self.arcdivision = arcdivision
         self.is_foam = is_foam
-    def is_lathe(self): return self.lathe_view_option
+
+    def is_lathe(self):
+        return self.lathe_view_option
+
 
 def sortByLine(elem):
     return elem["line"]
 
-class BackplotGenerator( rs274.glcanon.GlCanonDraw ):
 
+class BackplotGenerator(rs274.glcanon.GlCanonDraw):
     def __init__(self, inifile):
         self.inifile = linuxcnc.ini(inifile)
-        self.inifile_path = os.path.split(inifile)[0];
+        self.inifile_path = os.path.split(inifile)[0]
         self.select_primed = None
         rs274.glcanon.GlCanonDraw.__init__(self, linuxcnc.stat(), None)
         live_axis_count = 0
-        for i,j in enumerate("XYZABCUVW"):
-            if self.stat.axis_mask & (1<<i) == 0: continue
+        for i, j in enumerate("XYZABCUVW"):
+            if self.stat.axis_mask & (1 << i) == 0:
+                continue
             live_axis_count += 1
         self.num_joints = int(self.inifile.find("KINS", "JOINTS") or live_axis_count)
         self.foam_option = bool(self.inifile.find("DISPLAY", "FOAM"))
         temp = self.inifile.find("DISPLAY", "LATHE")
-        self.lathe_option = bool(temp == "1" or temp == "True" or temp == "true" )
+        self.lathe_option = bool(temp == "1" or temp == "True" or temp == "true")
         self.a_axis_wrapped = bool(self.inifile.find("AXIS_A", "WRAPPED_ROTARY"))
         self.b_axis_wrapped = bool(self.inifile.find("AXIS_B", "WRAPPED_ROTARY"))
         self.c_axis_wrapped = bool(self.inifile.find("AXIS_C", "WRAPPED_ROTARY"))
@@ -59,19 +70,36 @@ class BackplotGenerator( rs274.glcanon.GlCanonDraw ):
         self._current_file = filepath
         try:
             self.stat.poll()
-            random_toolchanger = int(self.inifile.find("EMCIO", "RANDOM_TOOLCHANGER") or 0)
+            random_toolchanger = int(
+                self.inifile.find("EMCIO", "RANDOM_TOOLCHANGER") or 0
+            )
             arcdivision = int(self.inifile.find("DISPLAY", "ARCDIVISION") or 64)
-            self.canon = StatCanon(None, self.inifile.find("DISPLAY", "GEOMETRY") or "XYZ", self.foam_option, self.lathe_option, self.stat, random_toolchanger, arcdivision)
-            parameter_file = os.path.join(self.inifile_path, os.path.basename(self.inifile.find("RS274NGC", "PARAMETER_FILE") or "linuxcnc.var"));
+            self.canon = StatCanon(
+                None,
+                self.inifile.find("DISPLAY", "GEOMETRY") or "XYZ",
+                self.foam_option,
+                self.lathe_option,
+                self.stat,
+                random_toolchanger,
+                arcdivision,
+            )
+            parameter_file = os.path.join(
+                self.inifile_path,
+                os.path.basename(
+                    self.inifile.find("RS274NGC", "PARAMETER_FILE") or "linuxcnc.var"
+                ),
+            )
             with tempfile.TemporaryDirectory() as tmpdirname:
-                tmp_parameter_file = os.path.join( tmpdirname, "backplot.var" )
+                tmp_parameter_file = os.path.join(tmpdirname, "backplot.var")
                 if os.path.exists(parameter_file):
                     shutil.copy(parameter_file, tmp_parameter_file)
                 self.canon.parameter_file = tmp_parameter_file
                 initcode = self.inifile.find("RS274NGC", "RS274NGC_STARTUP_CODE") or ""
                 result, seq = self.load_preview(filepath, self.canon, "G21", initcode)
                 if result > gcode.MIN_ERROR:
-                    print("In line {}, error: {}".format(str(seq), gcode.strerror(result)))
+                    print(
+                        "In line {}, error: {}".format(str(seq), gcode.strerror(result))
+                    )
         finally:
             pass
 
@@ -81,148 +109,107 @@ class BackplotGenerator( rs274.glcanon.GlCanonDraw ):
             currentline = self.canon.feed[0][0]
             feed = []
             for entry in self.canon.feed:
-                if (currentline != entry[0]):
-                    data.append({
-                        "type": "feed",
-                        "line": currentline,
-                        "feed" : feed
-                    })
+                if currentline != entry[0]:
+                    data.append({"type": "feed", "line": currentline, "feed": feed})
                     currentline = entry[0]
                     feed = []
-                feed.append({
-                    "coords": [
-                        entry[1][0],
-                        entry[1][1],
-                        entry[1][2],
-                        entry[2][0],
-                        entry[2][1],
-                        entry[2][2]
-                    ],
-                    "rate": entry[3],
-                    "offset": [
-                        entry[4][0],
-                        entry[4][1],
-                        entry[4][2]
-                    ]
-                })
-            data.append({
-                "type": "feed",
-                "line": currentline,
-                "feed" : feed
-            })
+                feed.append(
+                    {
+                        "coords": [
+                            entry[1][0],
+                            entry[1][1],
+                            entry[1][2],
+                            entry[2][0],
+                            entry[2][1],
+                            entry[2][2],
+                        ],
+                        "rate": entry[3],
+                        "offset": [entry[4][0], entry[4][1], entry[4][2]],
+                    }
+                )
+            data.append({"type": "feed", "line": currentline, "feed": feed})
 
         if self.canon.arcfeed and self.canon.arcfeed[0]:
             currentline = self.canon.arcfeed[0][0]
             arc = []
             for entry in self.canon.arcfeed:
-                if (entry[0] != currentline):
-                    data.append({
-                        "type": "arcfeed",
-                        "line": currentline,
-                        "arcfeed" : arc
-                    })
+                if entry[0] != currentline:
+                    data.append(
+                        {"type": "arcfeed", "line": currentline, "arcfeed": arc}
+                    )
                     currentline = entry[0]
                     arc = []
-                arc.append({
-                    "coords": [
-                        entry[1][0],
-                        entry[1][1],
-                        entry[1][2],
-                        entry[2][0],
-                        entry[2][1],
-                        entry[2][2]
-                    ],
-                    "rate": entry[3],
-                    "offset": [
-                        entry[4][0],
-                        entry[4][1],
-                        entry[4][2]
-                    ]
-                })
-            data.append({
-                "type": "arcfeed",
-                "line": currentline,
-                "arcfeed" : arc
-            })
+                arc.append(
+                    {
+                        "coords": [
+                            entry[1][0],
+                            entry[1][1],
+                            entry[1][2],
+                            entry[2][0],
+                            entry[2][1],
+                            entry[2][2],
+                        ],
+                        "rate": entry[3],
+                        "offset": [entry[4][0], entry[4][1], entry[4][2]],
+                    }
+                )
+            data.append({"type": "arcfeed", "line": currentline, "arcfeed": arc})
 
         if self.canon.traverse and self.canon.traverse[0]:
             currentline = self.canon.traverse[0][0]
             trav = []
             for entry in self.canon.traverse:
-                if (entry[0] != currentline):
-                    data.append({
-                        "type": "trav",
-                        "line": currentline,
-                        "trav" : trav
-                    })
+                if entry[0] != currentline:
+                    data.append({"type": "trav", "line": currentline, "trav": trav})
                     trav = []
                     currentline = entry[0]
-                trav.append({
-                    "coords": [
-                        entry[1][0],
-                        entry[1][1],
-                        entry[1][2],
-                        entry[2][0],
-                        entry[2][1],
-                        entry[2][2]
-                    ],
-                    "offset": [
-                        entry[3][0],
-                        entry[3][1],
-                        entry[3][2]
-                    ]
-                })
-            data.append({
-                "type": "trav",
-                "line": currentline,
-                "trav" : trav
-            })
+                trav.append(
+                    {
+                        "coords": [
+                            entry[1][0],
+                            entry[1][1],
+                            entry[1][2],
+                            entry[2][0],
+                            entry[2][1],
+                            entry[2][2],
+                        ],
+                        "offset": [entry[3][0], entry[3][1], entry[3][2]],
+                    }
+                )
+            data.append({"type": "trav", "line": currentline, "trav": trav})
 
         if self.canon.dwells and self.canon.dwells[0]:
             currentline = self.canon.dwells[0][0]
             trav = []
             for entry in self.canon.dwells:
-                if (entry[0] != currentline):
-                    data.append({
-                        "type": "dwell",
-                        "line": currentline,
-                        "trav" : trav
-                    })
+                if entry[0] != currentline:
+                    data.append({"type": "dwell", "line": currentline, "trav": trav})
                     trav = []
                     currentline = entry[0]
-                trav.append({
-                    "color": [
-                        entry[1]
-                    ],
-                    "coord": [
-                        entry[0],
-                        entry[1],
-                        entry[2]
-                    ]
-                })
-            data.append({
-                "type": "dwell",
-                "line": currentline,
-                "dwell" : trav
-            })
+                trav.append(
+                    {"color": [entry[1]], "coord": [entry[0], entry[1], entry[2]]}
+                )
+            data.append({"type": "dwell", "line": currentline, "dwell": trav})
 
         data.sort(key=sortByLine)
         rootData = {
-            "backplot" : data,
+            "backplot": data,
             "extents": [
                 self.canon.min_extents_zero_rxy[0],
                 self.canon.min_extents_zero_rxy[1],
                 self.canon.min_extents_zero_rxy[2],
                 self.canon.max_extents_zero_rxy[0],
                 self.canon.max_extents_zero_rxy[1],
-                self.canon.max_extents_zero_rxy[2]
-            ]
+                self.canon.max_extents_zero_rxy[2],
+            ],
         }
-        return json.dumps( rootData, separators=(',', ':') )
-        
-@app.route('/')
+        return json.dumps(rootData, separators=(",", ":"))
+
+
+@app.route("/")
 def index():
     return {"status": "OK!"}
+
 
 @app.put("/linuxcnc/backplot")
 def backplot():
@@ -230,7 +217,7 @@ def backplot():
         file_path = os.path.join(tmpdirname, "gcode.ngc")
         file = open(file_path, "w")
         gcode_bytes = base64.b64decode(request.json["gcode"])
-        gcode_string = gcode_bytes.decode('ascii');
+        gcode_string = gcode_bytes.decode("ascii")
         file.write(gcode_string)
         file.close()
         lathe_init_path = os.path.join(os.getcwd(), "lathe.ini")
@@ -238,6 +225,8 @@ def backplot():
         bp.load(file_path)
         return bp.toJson()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     from waitress import serve
-    serve(app, host='0.0.0.0', port=8001)
+
+    serve(app, host="0.0.0.0", port=8001)
--- elle-app/elle-hal/lathe_halcomp.py
+++ elle-app/elle-hal/lathe_halcomp.py
@@ -35,52 +35,58 @@ hal_pin_velocity_x_cmd = halc.newpin("velocity_x_cmd", hal.HAL_FLOAT, hal.HAL_OU
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return {"status": "OK!"}
 
+
 @app.get("/hal/hal_in")
 def read_hal_in():
-    return {"position_z": hal_pin_position_z.get(),
-            "position_x": hal_pin_position_x.get(),
-            "position_a": hal_pin_position_a.get(),
-            "speed_rps": hal_pin_speed_rps.get()}
+    return {
+        "position_z": hal_pin_position_z.get(),
+        "position_x": hal_pin_position_x.get(),
+        "position_a": hal_pin_position_a.get(),
+        "speed_rps": hal_pin_speed_rps.get(),
+    }
+
 
 @app.put("/hal/hal_out")
 def write_hal_out():
-    json = request.json;
+    json = request.json
 
     hal_pin_offset_z_encoder.set(-hal_pin_position_a.get())
     hal_pin_offset_z_stepper.set(+hal_pin_position_z.get())
     hal_pin_offset_x_encoder.set(-hal_pin_position_a.get())
     hal_pin_offset_x_stepper.set(+hal_pin_position_x.get())
 
-    if ("control_source" in json):
+    if "control_source" in json:
         hal_pin_control_source.set(json["control_source"])
-    if ("enable_stepper_z" in json):
+    if "enable_stepper_z" in json:
         hal_pin_enable_stepper_z.set(json["enable_stepper_z"])
-    if ("enable_stepper_x" in json):
+    if "enable_stepper_x" in json:
         hal_pin_enable_stepper_x.set(json["enable_stepper_x"])
 
-    if ("forward_z" in json):
+    if "forward_z" in json:
         hal_pin_forward_z.set(json["forward_z"])
-    if ("enable_z" in json):
+    if "enable_z" in json:
         hal_pin_enable_z.set(json["enable_z"])
-    if ("forward_x" in json):
+    if "forward_x" in json:
         hal_pin_forward_x.set(json["forward_x"])
-    if ("enable_x" in json):
+    if "enable_x" in json:
         hal_pin_enable_x.set(json["enable_x"])
 
-    if ("control_z_type" in json):
+    if "control_z_type" in json:
         hal_pin_control_z_type.set(json["control_z_type"])
-    if ("control_x_type" in json):
+    if "control_x_type" in json:
         hal_pin_control_x_type.set(json["control_x_type"])
-    if ("velocity_z_cmd" in json):
+    if "velocity_z_cmd" in json:
         hal_pin_velocity_z_cmd.set(json["velocity_z_cmd"])
-    if ("velocity_x_cmd" in json):
+    if "velocity_x_cmd" in json:
         hal_pin_velocity_x_cmd.set(json["velocity_x_cmd"])
 
-    return {"status" : "OK!"}
+    return {"status": "OK!"}
+
 
 halc.ready()
 
@@ -89,6 +95,7 @@ print("{REST_API_READY}")
 
 sys.stdout.flush()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from waitress import serve
-    serve(app, host='0.0.0.0', port=8000)
+
+    serve(app, host="0.0.0.0", port=8000)
