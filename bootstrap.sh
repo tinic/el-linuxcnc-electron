784e5360d60b19c8677e39de784ac074ced8b942
Tinic Uro <tinicuro@gmail.com>
2023-04-19 12:23:54 -0700

    Don't build during bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,4 @@ sudo apt install -y curl git
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo bash - &&\
 sudo apt install -y nodejs
 sudo apt install -y build-essential expect python3 linuxcnc-uspace linuxcnc-uspace-dev mesaflash python3-flask python3-flask-cors python3-waitress grub-customizer
-cd elle-app
 sudo npm install --global yarn
-yarn install
-yarn app:build
