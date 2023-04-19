c990a812e46ba15c0b34b5b821ccf759b2f8c81e
Tinic Uro <tinicuro@gmail.com>
2023-04-19 12:11:40 -0700

    Add -y

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-sudo apt install curl git
+sudo apt install -y curl git
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo bash - &&\
 sudo apt install -y nodejs
-sudo apt install build-essential expect python3 linuxcnc-uspace linuxcnc-uspace-dev mesaflash python3-flask python3-flask-cors python3-waitress grub-customizer
+sudo apt install -y build-essential expect python3 linuxcnc-uspace linuxcnc-uspace-dev mesaflash python3-flask python3-flask-cors python3-waitress grub-customizer
 cd elle-app
 sudo npm install --global yarn
 yarn install
