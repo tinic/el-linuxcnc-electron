be8cecc91de51c0c51f91b6773884c85e7a888e7
Tinic Uro <tinicuro@gmail.com>
2023-04-19 13:23:09 -0700

    Add yarn build back. Need more than 2GB of memory...

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,6 @@ curl -fsSL https://deb.nodesource.com/setup_18.x | sudo bash - &&\
 sudo apt install -y nodejs
 sudo apt install -y build-essential expect python3 linuxcnc-uspace linuxcnc-uspace-dev mesaflash python3-flask python3-flask-cors python3-waitress grub-customizer
 sudo npm install --global yarn
+cd elle-app
+yarn install
+yarn app:build
